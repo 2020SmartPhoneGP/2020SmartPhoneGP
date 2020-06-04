bafdf1d44bfc5529a32a92140b3a4e9c8295a57a
KimDongSoon <eutteumiyo@naver.com>
2020-06-04 22:13:51 +0900

    UI Base

---
+++ Team/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Team
+//
+//  Created by KPUGAME on 2020/06/01.
+//  Copyright © 2020 KPUGAME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
